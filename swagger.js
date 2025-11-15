17d8d05cadd4e06c5b957465a39233110e871144
Sunight00 <ephraim.ewane@gmail.com>
2025-11-15 22:16:36 +0100

    okay

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description:"Actors Api Documentation"
     },
     host:"localhost:8000",
-    schemes:["http", "https"]
+    schemes:["https"]
 }
 
 const outputFile = './swagger.json';
