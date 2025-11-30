9cf54e1fce612e1ccbe419fb1357fb437176511f
Sunight00 <ephraim.ewane@gmail.com>
2025-11-30 21:43:21 +0100

    okay

--- routes/directorsRoute.js
+++ routes/directorsRoute.js
@@ -7,6 +7,6 @@ router.get('/', directorsController.getAllDirectors);
 router.get('/:id', directorsController.getDirectorById);
 router.post('/',middleware.isAuthenticated, directorValidator.directorRules(), directorValidator.checkDirectorData, directorsController.createDirector);
 router.put('/:id',middleware.isAuthenticated,directorValidator.directorRules(), directorValidator.checkDirectorData, directorsController.updateDirector);
-router.delete('/:id',middleware.isAuthenticated, directorValidator.directorRules(), directorValidator.checkDirectorData, directorsController.deleteDirector);
+router.delete('/:id',middleware.isAuthenticated, directorsController.deleteDirector);
 
 module.exports = router;
